bea51bdaa137a0ba17f059214ff990b87e509518
Jonathan Colon <jonatron2.0@gmail.com>
2017-01-11 16:02:02 -0500

    labels to yellow

--- app/src/main/java/nyc/c4q/jonathancolon/inContaq/contactlist/fragments/ContactStatsFragment.java
+++ app/src/main/java/nyc/c4q/jonathancolon/inContaq/contactlist/fragments/ContactStatsFragment.java
@@ -179,7 +179,7 @@ public class ContactStatsFragment extends Fragment {
         mChart.setBorderSpacing(Tools.fromDpToPx(2))
                 .setAxisBorderValues(0, 75)
                 .setYLabels(AxisRenderer.LabelPosition.OUTSIDE)
-                .setLabelsColor(Color.parseColor("#6a84c3"))
+                .setLabelsColor(Color.parseColor("#FF9F1C"))
                 .setXAxis(false)
                 .setYAxis(false)
                 .setBackgroundColor(Color.parseColor("#052635"));
