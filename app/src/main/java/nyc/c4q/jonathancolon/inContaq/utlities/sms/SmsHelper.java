57a312f59b7aaecd307175f4dc8e4459821f6e3b
joncolon <jonatron2.0@gmail.com>
2017-03-15 14:56:10 -0400

    added bar graph

--- app/src/main/java/nyc/c4q/jonathancolon/inContaq/utlities/sms/SmsHelper.java
+++ app/src/main/java/nyc/c4q/jonathancolon/inContaq/utlities/sms/SmsHelper.java
@@ -223,6 +223,7 @@ public class SmsHelper {
         ArrayList<Sms> receivedSms = new ArrayList<Sms>();
 
         for (int i = 0; i < smsList.size(); i++) {
+            if (smsList.get(i).getType().equals("1")) {
                 receivedSms.add(smsList.get(i));
             }
         }
