83bdfb6aa158a73b14015ea5c6064ce16bacf464
wesniem <wesniem@yahoo.com>
2017-03-20 17:25:06 -0400

    Added splash screen

---
+++ app/src/main/java/nyc/c4q/jonathancolon/inContaq/contactlist/fragments/SplashScreenFragment.java
@@ -0,0 +1,23 @@
+package nyc.c4q.jonathancolon.inContaq.contactlist.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import nyc.c4q.jonathancolon.inContaq.R;
+
+
+public class SplashScreenFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_splash_screen,container,false);
+        return view;
+    }
+}
